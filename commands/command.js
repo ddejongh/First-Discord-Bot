d4a590e3506ec11216a769e64b8854a6311ea8a4
Devin DeJongh <devindejongh@gmail.com>
2020-12-21 20:06:19 -0600

    small fixes on command: command

--- commands/command.js
+++ commands/command.js
@@ -4,14 +4,15 @@ module.exports = {
     execute(message, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#ff2255')
-        .setTitle('Testing Embeds')
-        .setURL('https://youtube.com/PewDiePie')
+        .setTitle('My Github')
+        .setURL('https://github.com/ddejongh')
         .setDescription('This is an embed for testing embeds.')
         .addFields(
-            {name: 'PewDiePie #1', value: 'Brofist'}
+            {name: 'field1', value: 'From field one'}
         )
+        .setImage('https://i.pinimg.com/originals/fe/48/fb/fe48fbeb1a3bf62bf44c10882611017a.jpg')
         .setFooter('Test (from footer)');
 
-        message.channel.send();
+        message.channel.send(newEmbed);
     }  
 }
